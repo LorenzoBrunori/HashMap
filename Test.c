105865f494da0d2c861ec5a0e013adeed4c85780
Lorenzo Brunori <lorenzo.brunori97@gmail.com>
2019-01-04 15:20:22 +0100

    Add new tests

--- Test.c
+++ Test.c
@@ -41,6 +41,18 @@ TEST(MAPADD)
     free(key);
 }
 
+TEST(ADDLONGVALUE)
+{
+    _HashMap *new_Map = InitHashMap(SIZE);
+    char *key = _strdup("AIV");
+    long value = 658965896;
+
+    Add_Value(new_Map, key, (void *)&value);
+    ASSERT_THAT(new_Map->entries_used == 1);
+    Destroy(&new_Map);
+    free(key);
+}
+
 TEST(MAPADDCOLLISION)
 {
     _HashMap *new_Map = InitHashMap(SIZE);
@@ -75,16 +87,64 @@ TEST(GET)
     LinkedList *res = GetValue(new_Map, key);
 
     ASSERT_THAT(res->next == NULL);
-    ASSERT_THAT(*((int*)res->value) == 40);
+    ASSERT_THAT(*((int *)res->value) == 40);
 }
+
+TEST(POLL)
+{
+    _HashMap *new_Map = InitHashMap(SIZE);
+
+    char *key = _strdup("AIV");
+    int value = 40;
+
+    char *key2 = _strdup("HELLO");
+    int value2 = 37;
+
+    Add_Value(new_Map, key, (void *)&value);
+    Add_Value(new_Map, key2, (void *)&value2);
+
+    ASSERT_THAT(new_Map->entries_used == 2);
+
+    Poll(new_Map, key);
+    ASSERT_THAT(new_Map->entries_used == 1);
+
+    Destroy(&new_Map);
+    free(key);
+    free(key2);
+}
+
+TEST(CLEAN)
+{
+    _HashMap *new_Map = InitHashMap(SIZE);
+
+    char *key = _strdup("AIV");
+    int value = 40;
+
+    int value2 = 37;
+
+    Add_Value(new_Map, key, (void *)&value);
+    Add_Value(new_Map, key, (void *)&value2);
+    ASSERT_THAT(new_Map->entries_used == 1);
+
+    Clean(new_Map, key);
+    ASSERT_THAT(new_Map->entries_used == 0);
+
+    Destroy(&new_Map);
+    free(key);
+}
+
+
 
 int main(int argc, char **argv)
 {
     RUN_TEST(RandomTest);
     RUN_TEST(INITHASH);
     RUN_TEST(MAPADD);
+    RUN_TEST(ADDLONGVALUE);
     RUN_TEST(MAPADDCOLLISION);
     RUN_TEST(GET);
+    RUN_TEST(POLL);
+    RUN_TEST(CLEAN);
     PRINT_TEST_RESULTS();
     return 0;
 }
